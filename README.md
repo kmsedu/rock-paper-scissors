80f9e418bd6d81ecc2e5f8cbed19bfdcdcb01780
kmsedu <kmsedu@tuta.io>
2022-04-08 15:50:22 +1000

    Add images and credits for images to README.md Changes to ./index.html to accommodate ./styles/style.css Write UI logic in index.html & ./styles/style.css Rewrite JS game logic to handle UI

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A simple Rock, Paper, Scissors game written in JS and inside of script tags for ease of console use.
 
 To be updated with a UI at a later stage.
+
+[Rock Paper Scissors Vectors by Vecteezy](https://www.vecteezy.com/free-vector/rock-paper-scissors)
--- scripts/script.js
+++ scripts/script.js
@@ -2,34 +2,71 @@ const MOVES = ['ROCK', 'PAPER', 'SCISSORS'];
 const WINS = ['ROCKSCISSORS', 'PAPERROCK', 'SCISSORSPAPER'];
 let computerPlay = () => MOVES[Math.floor(Math.random() * 3)];
 let playerSelection;
-let playerWins;
-let computerWins;
+let playerWins = 0;
+let computerWins = 0;
 
-function getPlayerSelection() {
-    playerSelection = undefined;
+function displayChoices(playerSelection, computerSelection) {
+    const playerIcon = document.querySelector('#player-selection');
+    const computerIcon = document.querySelector('#computer-selection');
 
-    while(!playerSelection) {
-        playerSelection = prompt("Rock, Paper or Scissors?").toUpperCase();
-
-        if (!MOVES.includes(playerSelection)) {
-            alert("Invalid input, try again.")
-            playerSelection = undefined;
-        }
-    }
-    return playerSelection;
+    playerIcon.innerHTML = `<img src="./images/${playerSelection.toLowerCase()}.png">`
+    computerIcon.innerHTML = `<img src="./images/${computerSelection.toLowerCase()}.png">`
 }
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toUpperCase();
+    const score = document.getElementById('score');
+    const vsText = document.getElementById('vs-text');
+
+    vsText.textContent = `VS`
+    displayChoices(playerSelection, computerSelection);
 
     if (playerSelection == computerSelection) {
         return `Draw, you played ${playerSelection}, and it does nothing against the computer's ${computerSelection}!`;
     } else if (WINS.includes(playerSelection + computerSelection)) {
         playerWins++;
+        score.textContent = `Player: ${playerWins} | Computer: ${computerWins}`;
         return `Win, your ${playerSelection} beats the computer's ${computerSelection}!`;
     } else {
         computerWins++;
+        score.textContent = `Player: ${playerWins} | Computer: ${computerWins}`;
         return `Lose, the computer's ${computerSelection} beats your ${playerSelection}..`;
     }
-
 }
+
+function resetGame() {
+    playerWins = 0;
+    computerWins = 0;
+    playerSelection = undefined;
+
+    const playerIcon = document.querySelector('#player-selection');
+    const computerIcon = document.querySelector('#computer-selection');
+    const textWindow = document.querySelector('#text-window');
+    const score = document.getElementById('score');
+    const vsText = document.getElementById('vs-text');
+
+    vsText.textContent = ``
+    playerIcon.innerHTML = ``
+    computerIcon.innerHTML = ``
+    score.textContent = `Player: ${playerWins} | Computer: ${computerWins}`;
+    textWindow.textContent = ``
+    
+}
+
+const playerOptions = document.querySelectorAll('#player-options > button');
+if (!playerSelection) {
+    playerOptions.forEach(button => {
+        button.addEventListener('click', e => {
+            if (MOVES.some(ele => e.path[1].id.toUpperCase() == ele)) {
+                playerSelection = e.path[1].id.toUpperCase();
+                const textWindow = document.querySelector('#text-window');
+                textWindow.textContent = playRound(playerSelection, computerPlay());
+            }
+        });
+    });
+}
+
+const resetButton = document.querySelector('#reset-button');
+
+resetButton.addEventListener('click', resetGame);
+
